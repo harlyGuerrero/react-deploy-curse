42336c7b0bd8839802229becf944019de2c62d4a
Harly <harlydavinsonguerrero@gmail.com>
2026-03-23 22:43:31 -0500

    ultimo-commit

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -17,7 +17,7 @@ const TaskForm = () => {
     return(
         <div className="max-w-md mx-auto">
             <form onSubmit={handleSubmit} className="bg-slate-800 p-10 mb-4">
-                <h1 className="text-2xl font-bold text-white mb-3">Crea tu tarea Ahora</h1>
+                <h1 className="text-2xl font-bold text-white mb-3">Crea tu tarea</h1>
                 <input type="text" 
                 onChange={(e) => setTitle(e.target.value)}
                 placeholder="Ingrese la tarea"
@@ -29,7 +29,7 @@ const TaskForm = () => {
                 value={description} type="text"
                 className="bg-slate-300 p-3 w-full mb-2"
                 ></textarea>
-                <button className="bg-indigo-500 px-3 py-1 text-white" >Guardar</button>
+                <button className="bg-indigo-500 px-3 py-1 text-white" >Guardar Tarea</button>
             </form>
         </div>
     )
